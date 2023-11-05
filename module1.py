0e24ef127435b0880f0953baa4a8e4583861b561
Legolas1148 <niteeshrawal1@gmail.com>
2023-11-04 20:24:53 -0400

    UPDATED main spell checker module

--- module1.py
+++ module1.py
@@ -1,1 +1,21 @@
-
+from spellchecker import SpellChecker
+import pandas as pd
+sentence = input('Please enter a sentence = ')
+df = pd.read_excel(r'C:\Users\Legolas1148\Documents\MakeUC\Spell-Check\Spelling_Repository.xlsx')
+spellchecker = SpellChecker()
+words = sentence.split()
+dictionairy = set(df['Word'].str.lower())
+misspelled_words = []
+count = 0
+for word in words:
+    cleaned_word = ''.join(c for c in word if c.isalpha())
+    if misspelled_words == 0:
+        print("No misspelled words found.")
+    if cleaned_word.lower() not in dictionairy:
+        misspelled_words.append(cleaned_word)
+        print(spellchecker.correction(word.lower()))
+        print(spellchecker.candidates(word.lower()))
+    if len(misspelled_words) >= count:
+            print(f"The following words are misspelled: {', '.join(misspelled_words)}")
+            count = count + 1
+print("Thank you for your patronage")
