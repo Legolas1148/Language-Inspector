c3fe9550e10975c584fa5a0fb099a1c9406e5002
Legolas1148 <112130283+Legolas1148@users.noreply.github.com>
2023-11-05 04:06:47 -0500

    Update module1.py

--- module1.py
+++ module1.py
@@ -4,16 +4,18 @@ from PyDictionary import PyDictionary
 from googletrans import Translator
 dictionary= PyDictionary()
 translator = Translator()
-while True:
+while True:        #while loop that runs until the user quits the program
         choice = input('Do you want to access the dictionary, translator, or editor? (D/T/E) = ')
 
+        #Switch Case for choosing between Spell checker, dictionary, and  translation functionalities of the program
         match choice:
+                #Case for the Spelling Checker functionality of the program
                 case "E":
                     sentence = input('Please enter a sentence = ')
                     df = pd.read_excel(r'C:\Users\Legolas1148\Documents\MakeUC\Spell-Check\Spelling_Repository.xlsx')
                     spellchecker = SpellChecker()
                     words = sentence.split()
-                    dictionairy = set(df['Word'].str.lower())
+                    dictionary = set(df['Word'].str.lower())
                     misspelled_words = []
                     count = 0
                     for word in words:
@@ -27,11 +29,15 @@ while True:
                             print(f"The following words are misspelled: {', '.join(misspelled_words)}")
                     if len(misspelled_words) == 0:
                             print("No misspelled words found.")
+
+                #Case for Dictionary functionality of the program
                 case "D":
                     sentenceD = input('Please enter a word = ')
                     meaning= dictionary.meaning(sentenceD)
                     print(meaning)
-                case "T":
+
+                #Case for translator functionality of the program
+                case "T":        #Contains code for translating from english to spansih and vice versa
                     language = input('Please enter whether you want to translate to English or Spanish? (E/S) ')
                     if language == "E":
                         reply = input('Please enter a word = ')
@@ -47,7 +53,7 @@ while True:
                         translator = Translator()
                         translated_text = translator.translate(text = reply, dest = target_lang, src = source_lang)
                         print(f"Translated Text: {translated_text}")
-        answer = input('Do you want to continue? (Y/N) ')
+        answer = input('Do you want to continue? (Y/N) ')        #Condition that decides when the main program loop ends
         if answer == "Y" or answer == "y":
             continue
         else:
